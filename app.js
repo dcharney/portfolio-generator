71a65b8cb98a5e63d35f54b3ed36dd7c85b42931
dcharney <delaneycharney@gmail.com>
2020-12-15 16:05:58 -0800

    prompts user with questions

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-// link file system module
-const fs = require('fs');
+// // link file system module
+// const fs = require('fs');
 
-// import functions from src file
-const generatePage = require('./src/page-template.js');
+// link inquirer module
+const inquirer = require('inquirer');
 
-// gather input arguments from console
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// // import functions from src file
+// const generatePage = require('./src/page-template.js');
 
 
-// create an index file building the html using a function and console input args
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// // create an index file building the html using a function and console input args
+// const pageHTML = generatePage(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type:'input',
+        name:'name',
+        message:'What is your name?'
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+    });
