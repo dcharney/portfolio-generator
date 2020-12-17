8d55a659a763918723a44b96c74081739aaf0d18
dcharney <delaneycharney@gmail.com>
2020-12-17 15:25:13 -0800

    html generated from user inputs

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-// // link file system module
-// const fs = require('fs');
+// link file system module
+const fs = require('fs');
 
 // link inquirer module
 const inquirer = require('inquirer');
 
-// // import functions from src file
-// const generatePage = require('./src/page-template.js');
-
-
-// // create an index file building the html using a function and console input args
-// const pageHTML = generatePage(name, github);
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+// import functions from src file
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -146,5 +137,12 @@ const promptProject = portfolioData => {
 
 promptUser()
     .then(promptProject)
-    .then(portfolioData => {console.log(portfolioData)});
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
 
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
+    });
